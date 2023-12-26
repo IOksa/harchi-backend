2557fd3bf6672593fcc5327d0842f280d98b17da
IOksa <117656767+IOksa@users.noreply.github.com>
2023-12-26 22:00:16 +0200

    add environment variables

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
+const bodyParser = require("body-parser");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerDocument = require("./swagger.json");
+
+// const authRouter = require("./routes/auth");
+// auth google
+// const authGoogleRouter = require("./routes/authGoogleRouter");
+// const userRouter = require("./routes/users");
+// const recipesRouter = require("./routes/api/recipes");
+// const feedbacksRouter = require("./routes/api/feedbacks");
 
 const app = express();
 
@@ -10,6 +20,16 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+// app.use("/auth", authRouter);
+// app.use("/auth", authGoogleRouter);
+// app.use("/users", userRouter);
+// app.use("/recipes", recipesRouter);
+// app.use("/feedbacks", feedbacksRouter);
 
 app.use((req, res) => {
     res.status(404).json({ message: "Not found" });
